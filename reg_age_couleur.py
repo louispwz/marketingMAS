a4df75d35354ae665a5dbd0361039138f7c0a6c2
louispwz <louisdragnir@gmail.com>
2025-04-27 01:40:38 +0200

    Add files via upload

--- reg_age_couleur.py
+++ reg_age_couleur.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
