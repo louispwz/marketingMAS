45459d2488d1246abe8f5bccc72a3e8dcdcdfd3b
louispwz <louisdragnir@gmail.com>
2025-04-27 01:40:09 +0200

    Update reg_age_couleur.py

--- reg_age_couleur.py
+++ reg_age_couleur.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
